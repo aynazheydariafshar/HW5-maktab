3dedaa472a3b0e8c7a5fd8051af962973ffd6eab
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2021-12-07 14:20:49 +0330

    build algoritm max

--- ex2.js
+++ ex2.js
@@ -1,6 +1,13 @@
 function max(){
 
-
+    let maxelement = arguments[0]
+    for(let i =0 ; i<arguments.length ; i++){
+        if(arguments[i] > maxelement){
+            maxelement = arguments[i]
+        }
+    }
+    
+    return maxelement
 }
 
-console.log(max())
+console.log(max(4,5,7,8))
